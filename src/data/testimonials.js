ec6c0816387493445c4c7049ab9fb0ffef09bde8
Sri Shathurika <118598976+SriShathurika21ITR105@users.noreply.github.com>
2024-03-20 00:31:52 +0530

    Add files via upload

---
+++ src/data/testimonials.js
@@ -0,0 +1,19 @@
+let testimonials = [
+    {
+        id: 1,
+        user: "SERVICES",
+        comment:"At Sasti Furniture, we pride ourselves on offering a wide selection of high-quality furniture at affordable prices. With a commitment to quality assurance and convenient delivery services, we ensure that every piece of furniture you purchase meets your expectations. Our dedicated customer support team is always available to assist you, making your shopping experience with us hassle-free and enjoyable.",
+        image: "https://tse1.mm.bing.net/th?id=OIP.9maqMOMBVufi9Keq4rQxeQHaH_&pid=Api&rs=1&c=1&qlt=95&w=105&h=113",
+    }, {
+        id: 2,
+        user: "ABOUT US",
+        comment:"Welcome to Sasti Furniture, your destination for quality furniture at unbeatable prices. With a wide range of stylish and durable pieces, we're dedicated to helping you create a home you love without breaking the bank. Enjoy hassle-free shopping and exceptional service every step of the way. Discover affordable luxury with Sasti Furniture today!",
+        image: "https://tse1.mm.bing.net/th?id=OIP.oUHEVkWq9la01T-6BqzxmAHaE8&pid=Api&rs=1&c=1&qlt=95&w=117&h=78",
+    },{
+        user: "SECURITY",
+        comment:"Sasti Furniture prioritizes the security and privacy of our customers' information. We employ robust security measures to safeguard all data collected during transactions, ensuring confidentiality and protection against unauthorized access. Our website utilizes encryption protocols to secure online transactions, providing peace of mind to our customers when shopping with us. Additionally.Your trust and security are paramount to us at Sasti Furniture",
+        image: "https://cdn5.vectorstock.com/i/1000x1000/68/29/cyber-security-logo-design-vector-27946829.jpg",
+    }
+]
+
+export default testimonials;
